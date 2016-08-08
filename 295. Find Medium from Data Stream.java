bfb724270c0effb5a42c11696cac020475f7071a
Peter Jingshi <hanzi@utexas.edu>
2016-08-08 04:26:22 -0500

    Add comments

--- 295. Find Medium from Data Stream.java
+++ 295. Find Medium from Data Stream.java
@@ -1,1 +1,26 @@
+// https://leetcode.com/problems/find-median-from-data-stream/
+class MedianFinder {
 
+    // max queue is always larger or equal to min queue
+    PriorityQueue<Integer> min = new PriorityQueue();
+    PriorityQueue<Integer> max = new PriorityQueue(Collections.reverseOrder());
+    // Adds a number into the data structure.
+    public void addNum(int num) {
+        max.offer(num);
+        min.offer(max.poll());
+        if (max.size() < min.size()){
+            max.offer(min.poll());
+        }
+    }
+
+    // Returns the median of current data stream
+    public double findMedian() {
+        if (max.size() == min.size()) return (max.peek() + min.peek()) /  2.0;
+        else return max.peek();
+    }
+}
+
+// Your MedianFinder object will be instantiated and called as such:
+// MedianFinder mf = new MedianFinder();
+// mf.addNum(1);
+// mf.findMedian();
