bc28bd558fce1bde6a50657d7a030a23851da0a5
Peter Jingshi <hanzi@utexas.edu>
2016-08-08 04:21:17 -0500

    Create 146. LRU Cache.java

---
+++ 146. LRU Cache.java
@@ -0,0 +1,59 @@
+public class LRUCache {
+    int capacity;
+    HashMap<Integer, Node> map = new HashMap<Integer, Node>();
+    Node dummy = new Node(0,0);
+    class Node{
+        int key;
+        int val;
+        Node pre;
+        Node next;
+        public Node(int key, int value){
+            this.key = key;
+            this.val = value;
+        }
+    }
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        dummy.next = dummy;
+        dummy.pre = dummy;
+    }
+    
+    public int get(int key) {
+        if(map.containsKey(key)){
+            Node n = map.get(key);
+            remove(n);
+            setHead(n);
+            return n.val;
+        }
+        return -1;
+    }
+    
+    private void remove(Node n){
+        n.pre.next = n.next;
+        n.next.pre = n.pre;
+    }
+    
+    public void setHead(Node n){
+        n.next = dummy.next;
+        n.next.pre = n;
+        dummy.next = n;
+        n.pre = dummy;
+    }
+    
+    public void set(int key, int value) {
+        if(map.containsKey(key)){
+            Node old = map.get(key);
+            old.val = value;
+            remove(old);
+            setHead(old);
+        }else{
+            Node created = new Node(key, value);
+            if(map.size() >= capacity){
+                map.remove(dummy.pre.key);
+                remove(dummy.pre);
+            }
+            setHead(created);
+            map.put(key, created);
+        }
+    }
+}
